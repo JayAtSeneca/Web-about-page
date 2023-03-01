0cca49267fcaf8d77e315c076703ad3a1085d63e
JayAtSeneca <jpchaudhari@myseneca.ca>
2023-03-01 14:39:15 -0500

    update App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import styles from "./styles";
+import { CardDeal, CTA, Footer, Navbar, Overview } from "./components";
 
-
-function App() {
-  return (
-    <p>Hello World!</p>
-  );
-}
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+    
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Overview/>
+        <CardDeal/>
+        <CTA/>
+        {/* TODO: ADD More Info about Team members, Expertise and services, Unique value proposition, Awards and recognition, Company culture, Contact information */}
+        <Footer/>
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
