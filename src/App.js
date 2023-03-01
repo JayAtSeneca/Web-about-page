6b2441851ee534097c3a9b7b34510880d5c84619
JayAtSeneca <jpchaudhari@myseneca.ca>
2023-03-01 13:00:49 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <>
+    <header>
+      <link rel="icon" type="image/png" href="%PUBLIC_URL%/logo.png" className="rounded" />
+    </header>
+    <p>Hello World!</p>
+    </>
+  );
+}
+
+export default App;
