e73888f6844a2ce92d0a96776598003cc1f76a51
JayAtSeneca <jpchaudhari@myseneca.ca>
2023-03-01 14:40:48 -0500

    update assets index.js

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import Overview from "./Overview";
+import CardDeal from "./CardDeal";
+import CTA from "./CTA";
+import Footer from "./Footer";
+
+export {
+ Navbar,
+ Overview,
+ CardDeal,
+ CTA,
+ Footer
+};
